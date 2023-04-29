5f940da488d08ad398e63229bb291f9f3b9991a1
eldarkann <eldar.kann@gmail.com>
2023-04-29 23:38:22 +0200

    Fixed error in platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -9,9 +9,10 @@ interface Props {
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = usePlatforms();
+  const { data, error } = usePlatforms();
 
   if (error) return null;
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
